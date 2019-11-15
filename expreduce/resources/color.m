ac6029782bb68d7db382e218926a6e417fc77301
Werner Laurensse <werner@alman.ax>
2019-11-15 20:22:48 +0100

    Add buildin colors.

---
+++ expreduce/resources/color.m
@@ -0,0 +1,16 @@
+Black = RGBColor[0,0,0];
+White = RGBColor[1,1,1];
+Red = RGBColor[1,0,0];
+Green = RGBColor[0,1,0];
+Blue = RGBColor[0,0,1];
+Yellow = RGBColor[1,1,0];
+Cyan = RGBColor[0,1,1];
+Magenta = RGBColor[1,0,1];
+Brown = RGBColor[0.6,0.4,0.2];
+Orange = RGBColor[1,0.5,1];
+Pink = RGBColor[1,0.5,0.5];
+Purple = RGBColor[0.5,0,0.5];
+Grey = RGBColor[0.5];
+
+Thick = AbsoluteThickness[0.8];
+Thin = AbsoluteThickness[0.2];
