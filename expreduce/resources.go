14edbcad00df1631752874ee43bca17272e99730
Werner Laurensse <werner@alman.ax>
2019-12-12 04:54:03 +0100

    Update expreduce resource files.

--- expreduce/resources.go
+++ expreduce/resources.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42b3910e4654d0755c195643af1eafef9312800c9ddd5ad75b0bfe74905b8c9f
-size 1342570
+oid sha256:5ec86fa33af1e627ee1db9c20abb4c986697810ba2d8312a6b84e5e4b231f7db
+size 1349654
